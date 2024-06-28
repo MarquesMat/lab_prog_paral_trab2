306e90dce5fdeb4031bf4efb3047b8995938583c
Mauro <43522127+MGsBueno@users.noreply.github.com>
2024-06-28 01:58:27 -0300

    refactor: remoção de if's redundantes.

--- omp_primos_bag.c
+++ omp_primos_bag.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
-#define TAMANHO 500000
 
+#define TAMANHO 500000
 
 // Retorna 1 se n for primo e 0 se não for
 int primo(long int n) {
@@ -17,11 +17,19 @@ int primo(long int n) {
     return 1;
 }
 
+// Retorna o tempo marcado pela thread que terminou por último
+double maior_tempo_final(int num_threads, double tempos[]) {
+    double maior = tempos[0];
+    for (int i = 1; i < num_threads; i++) {
+        if (tempos[i] > maior) maior = tempos[i];
+    }
+    return maior;
+}
+
 int main(int argc, char *argv[])
 {
-    double t_inicial, t_final;
-    int total = 0;
-    int n;
+    double t_inicio;
+    long int n, total = 0;
     int num_threads;
     int tam = TAMANHO;
 
@@ -38,44 +46,38 @@ int main(int argc, char *argv[])
 
     /* Define o número de threads */
     num_threads = omp_get_max_threads();
+    double t_fim[num_threads]; // Declare array after num_threads is set
 
-       while (tam * num_threads >= n)
+    while (tam > 50000 && tam * num_threads >= n )
     {
         tam -= 50000;
-        
     }
-    if (tam == 0 ) tam = 1;
-
+    
     /* Registra o tempo inicial de execução do programa */
-    t_inicial = omp_get_wtime();
+    t_inicio = omp_get_wtime();
 
     /* Inicia a região paralela */
-    #pragma omp parallel num_threads(num_threads) shared(total, n)
+    #pragma omp parallel num_threads(num_threads) shared(total, n, tam, t_fim)
     {
         int tid = omp_get_thread_num();
-        int inicio, fim;
+        double thread_start_time = omp_get_wtime();
 
         /* Thread mestre (main) carrega a bag de tarefas */
-        #pragma omp single
+        #pragma omp single nowait 
         {
-            inicio = 3;
+            int inicio = 3;
             while (inicio <= n)
             {
-                #pragma omp task firstprivate(inicio) firstprivate(fim) if (tid != 0)
+                #pragma omp task firstprivate(inicio) private(tid)
                 {
                     int local_count = 0;
-                    fim = inicio + tam-1;
-                    if (fim > n) fim = n;
-                    if (fim<inicio) fim = inicio;
-
-                    for (int i = inicio; i <= fim; i += 2)
+                    int fim = inicio + tam - 1;
+                    if (fim < inicio) fim = inicio;
+                    for (int i = inicio; i <= fim && i < n; i += 2)
                     {
-                        
                         if (primo(i))
                             local_count++;
-                        
                     }
-
                     #pragma omp atomic
                     total += local_count;
                 }
@@ -83,15 +85,16 @@ int main(int argc, char *argv[])
             }
         } // Fim da região single
 
+        t_fim[tid] = omp_get_wtime() - thread_start_time;
     } // Fim da região paralela
 
-    /* Registra o tempo final de execução */
-    t_final = omp_get_wtime();
+    // Acrescenta o número 2, que não foi verificado pelo loop
+    if (n >= 2) total++;
 
-    /* Imprime o resultado */
-    total += 1; // Adiciona o número 2, que é primo
-    printf("Quantidade de primos entre 1 e %d: %d\n", n, total);
-    printf("Tempo de execução: %1.3f segundos\n", t_final - t_inicial);
+    double t_total = maior_tempo_final(num_threads, t_fim);
+
+    printf("Quantidade de primos entre 1 e %ld: %ld \n", n, total);    
+    printf("Tempo total de execução: %3.10f segundos\n\n", t_total);
 
     return 0;
 }
